2702e681d26e7b5ff0074f564f295713da5ead57
talhabutt7 <talhabuttt@gmail.com>
2022-02-28 17:20:54 +0000

    Week4 Ex6, Added mouse enter and leave event on elements to change content on hover.

--- week-4/wk4-ex6.js
+++ week-4/wk4-ex6.js
@@ -8,11 +8,26 @@ function clickparaFunction(){
     element.firstChild.nodeValue = 'This is new updated paragragh. Thanks to JS. :)'
 }
 
+function oldFunction(){
+    var element = document.getElementById('main_heading');
+    element.firstChild.nodeValue = 'Page Heading'
+}
+
+function oldparaFunction(){
+    var element = document.getElementById('paragraph');
+    element.firstChild.nodeValue = 'This is old hard coded paragraph. Please change me!'
+}
+
 function headingChange(){
     var element = document.getElementById('main_heading');
-    element.addEventListener('click', clickFunction)
+    element.addEventListener('mouseenter', clickFunction)
     var element = document.getElementById('paragraph');
-    element.addEventListener('click', clickparaFunction)
+    element.addEventListener('mouseenter', clickparaFunction)
+
+    var element = document.getElementById('main_heading');
+    element.addEventListener('mouseout', oldFunction)
+    var element = document.getElementById('paragraph');
+    element.addEventListener('mouseout', oldparaFunction)
 }
 
 
