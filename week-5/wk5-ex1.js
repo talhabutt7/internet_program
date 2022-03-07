536ff422912d586e846b9978f5c1f294aa970331
talhabutt7 <talhabuttt@gmail.com>
2022-03-07 16:08:07 +0000

    Week5-Ex2: Opacity is 0.5 on document load and after clicking the button it is 1

--- week-5/wk5-ex1.js
+++ week-5/wk5-ex1.js
@@ -3,10 +3,12 @@ function alertCircle(){
     var element = document.getElementById('circle');
     alert('Circle was clicked. Click OK below and circle color will change into Blue.');
     element.style.backgroundColor = 'blue';
+    element.style.opacity = '1';
 }
 
 function onLoad(){
     var element = document.getElementById('circle');
+    element.style.opacity = '0.5';
     element.addEventListener('click', alertCircle)
 }
 
