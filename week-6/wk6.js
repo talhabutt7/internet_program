bad7ba4a3a2b235284e76167a1f6b85fe8c486e5
talhabutt7 <talhabuttt@gmail.com>
2022-03-14 15:40:25 +0000

    Week6-Ex1: A vertical bar chart and pie chart using "chart.js"

---
+++ week-6/wk6.js
@@ -0,0 +1,57 @@
+var ctx = document.getElementById('myChart');
+var ctxPie = document.getElementById('pieChart');
+var stars = [95, 90, 70, 85, 55];
+var frameworks = ['Internet Programming', 'Databases', 'Intelligent System', 'Computer Architecture', 'Java'];
+
+var myChart = new Chart(ctx, {
+ type: 'bar',
+ data: {
+    labels: frameworks,
+    datasets: [{
+        label: 'MSc Computing Marks',
+        data: stars,
+        backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)"
+            ],
+            borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+            borderWidth: 1
+    }]
+ },
+})
+
+
+var pieChart = new Chart(ctxPie, {
+    type: 'pie',
+    data: {
+       labels: frameworks,
+       datasets: [{
+           label: 'MSc Computing Marks',
+           data: stars,
+           backgroundColor: [
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(54, 162, 235, 0.2)",
+               "rgba(255, 206, 86, 0.2)",
+               "rgba(75, 192, 192, 0.2)",
+               "rgba(153, 102, 255, 0.2)"
+               ],
+               borderColor: [
+             "rgba(255, 99, 132, 1)",
+             "rgba(54, 162, 235, 1)",
+             "rgba(255, 206, 86, 1)",
+             "rgba(75, 192, 192, 1)",
+             "rgba(153, 102, 255, 1)",
+           ],
+               borderWidth: 1
+       }]
+    },
+   })
