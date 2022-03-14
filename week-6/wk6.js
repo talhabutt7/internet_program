6f14b3f5e396924ddfc9a4ee703fbc95a995e878
talhabutt7 <talhabuttt@gmail.com>
2022-03-14 15:52:40 +0000

    Week6: Bar, Pie and Line chart are all added in the same page using chart.js

--- week-6/wk6.js
+++ week-6/wk6.js
@@ -1,9 +1,12 @@
-var ctx = document.getElementById('myChart');
-var ctxPie = document.getElementById('pieChart');
+var bar = document.getElementById('myChart');
+var pie = document.getElementById('pieChart');
+var line = document.getElementById('lineChart');
+
 var stars = [95, 90, 70, 85, 55];
 var frameworks = ['Internet Programming', 'Databases', 'Intelligent System', 'Computer Architecture', 'Java'];
 
-var myChart = new Chart(ctx, {
+// BAR CHART
+var barChart = new Chart(bar, {
  type: 'bar',
  data: {
     labels: frameworks,
@@ -27,10 +30,22 @@ var myChart = new Chart(ctx, {
             borderWidth: 1
     }]
  },
+ options: {
+    scales: {
+        yAxes: [{
+            display: true,
+            stacked: true,
+            ticks: {
+                min: 0, // minimum value
+                max: 100 // maximum value
+            }
+        }]
+    }
+}
 })
 
-
-var pieChart = new Chart(ctxPie, {
+// PIE CHART
+var pieChart = new Chart(pie, {
     type: 'pie',
     data: {
        labels: frameworks,
@@ -54,4 +69,43 @@ var pieChart = new Chart(ctxPie, {
                borderWidth: 1
        }]
     },
+   })
+
+   // LINE CHART
+   var lineChart = new Chart(line, {
+    type: 'line',
+    data: {
+       labels: frameworks,
+       datasets: [{
+           label: 'MSc Computing Marks',
+           data: stars,
+           backgroundColor: [
+               "rgba(255, 99, 132, 0.2)",
+               "rgba(54, 162, 235, 0.2)",
+               "rgba(255, 206, 86, 0.2)",
+               "rgba(75, 192, 192, 0.2)",
+               "rgba(153, 102, 255, 0.2)"
+               ],
+               borderColor: [
+             "rgba(255, 99, 132, 1)",
+             "rgba(54, 162, 235, 1)",
+             "rgba(255, 206, 86, 1)",
+             "rgba(75, 192, 192, 1)",
+             "rgba(153, 102, 255, 1)",
+           ],
+               borderWidth: 1
+       }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                display: true,
+                stacked: true,
+                ticks: {
+                    min: 0, // minimum value
+                    max: 100 // maximum value
+                }
+            }]
+        }
+    }
    })
